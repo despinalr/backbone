2416ff50e344d42ea828e2eb58b1ad967e1b1777
David Espinal Ruiz <davidesr7@gmail.com>
2013-09-02 16:59:11 -0500

    primer commit
    primer commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).on("ready", function() {
+	$("#conectar").click(function() {
+		alert("hola mundo");
+	});		
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var swig    = require('swig');
+var cons    = require('consolidate');
+var path	= require('path');
+
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(app.router);
+
+	//Registra Contenido Estático y Vistas
+	app.engine('.html', cons.swig);
+	app.set('view engine', 'html');
+	app.use(express.static('./js'));
+	app.use(express.static('./js/lib'));
+	app.use(express.static('./js/models'));
+	app.use(express.static('./js/views'));
+});
+
+require('./Rest/routes.js')(app);
+
+server.listen(3000);
