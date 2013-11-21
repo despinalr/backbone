6975cd41ff2e69fccfdb996fc91786313d1ecee4
David Espinal Ruiz <davidesr7@gmail.com>
2013-11-21 16:03:47 -0500

    mas pruebas

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var mongo = require('./../Rest/mongoose');
 var assert = require("assert");
 var should = require('should');
+var request = require('request');
 
 'use strict';
 
@@ -33,7 +34,6 @@ describe('Array', function() {
 });
 
 describe('mongoose getRecord', function() {
-
     before(function(done) {
         var record = "{ \"Nombre\": \"Michael\", \"Apellido\": \"Myers\", \"Cedula\": 123 }";
         mongo.insertRecord(JSON.parse(record), function(response) {
@@ -53,6 +53,42 @@ describe('mongoose getRecord', function() {
     after(function(done) {
         mongo.deleteRecord("Cedula", 123, function(response) {
             done();
+        });
+    });
+});
+
+describe('rest getAllRecord', function() {
+    describe('getAllRecord', function() {
+        it('should be json', function(done) {
+            request("http://localhost:3000/personas", function(error, response, body) {
+                response.should.be.json;
+                done();
+            });
+        });
+
+        it('should return 5 records', function(done) {
+            request("http://localhost:3000/personas", function(error, response, body) {
+                var json = JSON.parse(body);
+                assert.equal(5, json.length);
+                done();
+            });
+        });
+
+        it('should save a record', function(done) {
+            request({
+                url: 'http://localhost:3000/personas',
+                method: 'post',
+                json: { Nombre: 'Randy', Apellido: 'Couture', Cedula: 123 }
+            }, function(error, response, body) {
+                body.status.should.equal('ok');
+                done();
+            });
+        });
+
+        after(function(done) {
+            mongo.deleteRecord("Cedula", 123, function(response) {
+                done();
+            });
         });
     });
 });
